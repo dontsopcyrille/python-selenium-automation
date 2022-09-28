d5ff6f107fab6e84abfd7f465e8d189e7d5a5f68
dontsopcyrille <c.dontsop@yahoo.fr>
2022-09-27 19:56:50 -0500

    changes per hw2

--- features/hw2_test_cases.py
+++ features/hw2_test_cases.py
@@ -7,4 +7,10 @@ driver.find_element(By.ID, 'twotabsearchtextbox').send_keys('soccer ball')
 driver.find_element(By.ID, 'nav-search-submit-button').click()
 driver.find_element(By.XPATH, "//a[@class='a-link-normal s-color-swatch-link puis-spacing-small s-hidden-in-quick-view']").click()
 driver.find_element(By.ID, 'buy-now-button').click()
-g
+
+expected_result = 'Sign in'
+actual_result = driver.find_element(By.XPATH, "//*[@class='a-spacing-small']").text
+assert expected_result==actual_result, f'Error! Expected {expected_result}, but got {actual_result}'
+print('Test case passed')
+
+
