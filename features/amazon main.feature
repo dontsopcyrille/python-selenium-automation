bef92cb097816f1d14d53d2f61dfa6d90c1c7edd
dontsopcyrille <c.dontsop@yahoo.fr>
2022-10-26 14:29:31 -0500

    per hwk HTML&CSS PART2

---
+++ features/amazon main.feature
@@ -0,0 +1,5 @@
+# Created by donts at 10/6/2022
+Feature: test for amazon main page
+  Scenario: # Enter scenario name here
+   Given  open amazon page
+    Then  verify hamburger menu is present
--- features/steps/Amazon_main_steps.py
+++ features/steps/Amazon_main_steps.py
@@ -3,5 +3,5 @@ from behave import given, when, then
 
 @when('search for coffee')
 def search_product(context):
-    context.driver.find_element(By.ID, 'twotabsearchtextbox').send_keys('coffee')
-    context.driver.find_element(By.ID, 'nav-search-submit-button').click()
+ #   context.driver.find_element(By.ID, 'twotabsearchtextbox').send_keys('coffee')
+  #  context.driver.find_element(By.ID, 'nav-search-submit-button').click()
