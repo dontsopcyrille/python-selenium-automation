079a7f6a6fc12cbb9c815124371e88f84e29f9fb
dontsopcyrille <c.dontsop@yahoo.fr>
2022-11-08 17:06:16 -0600

    changes per hw3

--- features/hw3_test_cases.py
+++ features/hw3_test_cases.py
@@ -1,12 +1,36 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
+# Find Amazon registration logo By class
+driver.find_element(By.CSS_SELECTOR, ".a-icon.a-icon-logo")
 
-driver = webdriver.Chrome(executable_path="C:\\Users\\donts\\Desktop\\Automation\\python-selenium-automation\\chromedriver.exe")
-driver.get('https://www.amazon.com/')
-driver.find_element(By.CSS_SELECTOR, "#nav-cart-count").click()
-expected_result = 'Your Amazon Cart is empty'
-actual_result = driver.find_element(By.CSS_SELECTOR, "[class='a-row sc-your-amazon-cart-is-empty']").text
-assert expected_result==actual_result, f'Error! Expected {expected_result}, but got {actual_result}'
-print('Test case passed')
+# Find Amazon create account element By class + class
+driver.find_element(By.CSS_SELECTOR, "h1.a-spacing-small")
+
+# Find Amazon your name bar element By ID
+driver.find_element(By.CSS_SELECTOR, "#ap_customer_name")
+
+# Find Amazon email element By attribute
+driver.find_element(By.CSS_SELECTOR, "[for='ap_email']")
+
+# Find Amazon password bar element By attribute
+driver.find_element(By.CSS_SELECTOR, "[name='password']")
+
+# Find Amazon Re-enter password bar element By ID
+driver.find_element(By.CSS_SELECTOR, "#ap_password_check")
+
+# Find  Amazon continue(create your Amazon account) bar element By ID
+driver.find_element(By.CSS_SELECTOR, "#continue")
+
+# Find  Amazon conditions of use By partial attribute
+driver.find_element(By.CSS_SELECTOR, "a[href*='/gp/help/customer/display.html/ref=ap_register_notification_condition_of_use']")
+
+# Find Amazon privacy notice bar element By ID
+driver.find_element(By.CSS_SELECTOR, "#legalTextRow")
+
+# Find Amazon sign in  element By attribute
+driver.find_element(By.CSS_SELECTOR, "[class='a-link-emphasis']")
+
+
+
+
+
 
 
