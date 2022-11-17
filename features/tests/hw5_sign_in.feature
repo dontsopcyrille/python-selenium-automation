2a79c936051de05ced9cc6cf0012e403f280d68f
dontsopcyrille <c.dontsop@yahoo.fr>
2022-11-16 19:30:47 -0600

    cpommit per hw5

---
+++ features/tests/hw5_sign_in.feature
@@ -0,0 +1,6 @@
+Feature: Sign in test cases
+
+  Scenario: Sign In page can be opened from SignIn popup
+    Given Open Amazon page
+    When Click on button from SignIn popup
+    Then Verify Sign in page opened
---
+++ hw5_sample_script_py.py
@@ -0,0 +1,35 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+# init driver
+driver = webdriver.Chrome()
+driver = webdriver.Chrome(executable_path="C:\\Users\\donts\\Desktop\\Automation\\python-selenium-automation\\chromedriver.exe")
+driver.maximize_window()
+driver.implicitly_wait(5) # applied to find_element(s) / #100 ms if element is found
+driver.wait = WebDriverWait(driver, 10)
+
+# open the url
+driver.get('https://www.google.com/')
+
+search = driver.find_element(By.NAME, 'q')
+search.clear()
+
+search.send_keys('Car')
+
+# wait for 4 sec
+#sleep(4)
+btn = (By.NAME, 'btnK')
+driver.wait.until(EC.el   nt_to_be_clickable(btn), message='Search btn not clickable')  # 500 ms
+
+# click search
+driver.find_element(By.NAME, 'btnK').click()
+
+# verify
+assert 'dress' in driver.current_url.lower(), f"Expected query not in {driver.current_url.lower()}"
+assert 'car' in driver.current_url.lower(), f"Expected query not in {driver.current_url.lower()}"
+print('Test Passed')
+
+driver.quit()
